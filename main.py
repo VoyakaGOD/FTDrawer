ae58c7dc39c0b6b20e8ca9ea6318e9c38238302f
VoyakaGOD <ab3000645@gmail.com>
2025-01-04 06:07:39 +0300

    main.py was tidied up

--- main.py
+++ main.py
@@ -7,46 +7,50 @@ from camera import Camera
 import pygame as pg
 from sys import argv
 
-pg.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("FTDrawer")
-clock = pg.time.Clock()
-running = True
+def main():
+    # Check args:
+    if len(argv) < 4:
+        print("Usage `python main.py [filename] [n] [f1]`")
+        exit(-1)
+    # Init pygame:
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    pg.display.set_caption("FTDrawer")
+    clock = pg.time.Clock()
+    # Init main objects:
+    filename = argv[1]
+    n = int(argv[2])
+    f1 = float(argv[3])
+    description = get_first_path_description(filename)
+    description.fit_in(Camera.get_screen_size())
+    gismos_drawer = GismosSystem(Camera.get_canvas_center(), f1, get_coefficients(description, n))
+    path_drawer = PathDrawer(gismos_drawer.get_point(), PATH_BRIGHT_PART / f1)
+    camera = Camera()
+    # Main loop:
+    running = True
+    while running:
+        # Events:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+            elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                running = False
+            else:
+                camera.handle_event(event)
+        # Update:
+        delta_time = clock.get_time() / 1e3 # in seconds
+        gismos_drawer.update(delta_time)
+        path_drawer.add_point(gismos_drawer.get_point())
+        camera.update(delta_time)
+        path_drawer.update_tail_time(delta_time)
+        # Draw:
+        path_drawer.draw(camera.get_canvas())
+        gismos_drawer.draw(camera.get_canvas())
+        camera.draw(screen)
+        pg.display.flip()
+        clock.tick(60)
 
-if len(argv) < 4:
-    print("Usage `python main.py [filename] [n] [f1]`")
-    exit(-1)
 
-f1 = float(argv[3])
-
-description = get_first_path_description(argv[1])
-description.fit_in(Camera.get_screen_size())
-bounding_rect = description.get_bounding_rect().move(Camera.get_canvas_center())
-gismos_drawer = GismosSystem(Camera.get_canvas_center(), f1, get_coefficients(description, int(argv[2])))
-path_drawer = PathDrawer(gismos_drawer.get_point(), PATH_BRIGHT_PART / f1)
-camera = Camera()
-
-while running:
-    # Events:
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            running = False
-        elif event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
-            running = False
-        else:
-            camera.handle_event(event)
-    # Update:
-    delta_time = clock.get_time() / 1e3 # in seconds
-    gismos_drawer.update(delta_time)
-    path_drawer.add_point(gismos_drawer.get_point())
-    camera.update(delta_time)
-    path_drawer.update_tail_time(delta_time)
-    # Draw:
-    path_drawer.draw(camera.get_canvas())
-    gismos_drawer.draw(camera.get_canvas())
-    # todo: fix, pg.draw.rect(camera.get_canvas(), (255, 0, 0), bounding_rect, 3)
-    camera.draw(screen)
-    pg.display.flip()
-    clock.tick(60)
-
-pg.quit()
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
